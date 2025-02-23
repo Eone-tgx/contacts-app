c30856412af760e82272fa955d45ef60456383e8
Eone-tgx <eone1024@gmail.com>
2025-02-23 16:49:52 +0200

    1

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  LS_KEY: "token",
+  loginBtn: document.querySelector(".login-btn"),
+  signUpBtn: document.querySelector(".sign-up-btn"),
+  contactsForm: document.querySelector(".contact-form"),
+  logoutBtn: document.querySelector(".log-out-btn"),
+  contactsList: document.querySelector(".contacts-list"),
+};
--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+import "./js/registration";
+import "./js/login";
+import { refs } from "./js/refs";
 
+const token = localStorage.getItem(refs.LS_KEY);
+
+if (token) {
+  location.replace("/contacts.html");
+}
